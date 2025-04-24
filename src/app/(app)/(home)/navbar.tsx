df4f137bdc98c3bcb7e6286b4639ec89f5cbb616
SUGATACHANDA <scbabai2704@gmail.com>
2025-04-24 23:09:29 +0530

    feat(auth): implement sign-in and sign-up pages with validation schemas and TRPC procedures
    - Added SignInView and SignUpView components for user authentication.
    - Created login and registration schemas using Zod for input validation.
    - Implemented TRPC procedures for user session management, login, and registration.
    - Introduced AUTH_COOKIE constant for cookie management.
    - Enhanced user experience with form handling and error messages using react-hook-form and sonner for notifications.

--- src/app/(app)/(home)/navbar.tsx
+++ src/app/(app)/(home)/navbar.tsx
@@ -87,7 +87,7 @@ export const Navbar = () => {
                     variant="secondary"
                     className='border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg'
                 >
-                    <Link href="/sign-in">
+                    <Link prefetch href="/sign-in">
                         Log In
                     </Link>
 
@@ -96,7 +96,7 @@ export const Navbar = () => {
                     asChild
                     className='border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:text-black hover:bg-pink-400 transition-colors text-lg'
                 >
-                    <Link href="/sign-up">
+                    <Link prefetch href="/sign-up">
                         Start selling
                     </Link>
                 </Button>
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,8 +1,16 @@
+"use client"
+
+import { useTRPC } from "@/trpc/client"
+import { useQuery } from "@tanstack/react-query"
+
 export default function Home() {
+
+  const trpc = useTRPC()
+  const { data } = useQuery(trpc.auth.session.queryOptions())
 
   return (
     <div className="p-4">
-      Home Page
+      {JSON.stringify(data?.user, null, 2)}
     </div>
   )
 }
