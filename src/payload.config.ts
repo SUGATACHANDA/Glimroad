359c4d3c7208d337f780ed76fb567855e5a7769a
SUGATACHANDA <scbabai2704@gmail.com>
2025-05-09 22:47:33 +0530

    feat: add product view and star rating components, implement currency formatting, and enhance product procedures

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -14,6 +14,7 @@ import { Products } from './collections/Products'
 import { Tags } from './collections/Tags'
 import { Tenants } from './collections/Tenants'
 import { multiTenantPlugin } from "@payloadcms/plugin-multi-tenant"
+import { Config } from './payload-types'
 
 
 const filename = fileURLToPath(import.meta.url)
@@ -38,7 +39,7 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    multiTenantPlugin({
+    multiTenantPlugin<Config>({
       collections: {
         products: {},
       },
