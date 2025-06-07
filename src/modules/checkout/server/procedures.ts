50c7e22d82ab09d561d7770427b590fa977a477f
SUGATACHANDA <scbabai2704@gmail.com>
2025-06-07 14:28:15 +0530

    feat: update tenant URL generation and enhance middleware for tenant routing

--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -10,6 +10,7 @@ import { z } from "zod";
 import { CheckoutMetadata, ProductMetadata } from "../types";
 import { stripe } from "@/lib/stripe";
 import { PLATFORM_FEE_PERCENT } from "@/constants";
+import { generateTenantURL } from "@/lib/utils";
 
 export const checkoutRouter = createTRPCRouter({
     verify: protectedProcedure.mutation(async ({ ctx }) => {
@@ -144,11 +145,13 @@ export const checkoutRouter = createTRPCRouter({
                 totalAmount * (PLATFORM_FEE_PERCENT / 100)
             )
 
+            const domain = generateTenantURL(input.tenantSlug)
+
             const checkout = await stripe.checkout.sessions.create({
                 customer_email: ctx.session.user.email,
                 // billing_address_collection: "required",
-                success_url: `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${input.tenantSlug}/checkout?success=true`,
-                cancel_url: `${process.env.NEXT_PUBLIC_APP_URL}/tenants/${input.tenantSlug}/checkout?cancel=true`,
+                success_url: `${domain}/checkout?success=true`,
+                cancel_url: `${domain}/checkout?cancel=true`,
                 mode: "payment",
                 line_items: lineItems,
                 invoice_creation: {
--- src/modules/tenants/ui/components/footer.tsx
+++ src/modules/tenants/ui/components/footer.tsx
@@ -12,7 +12,7 @@ export const Footer = () => {
         <footer className="border-t font-medium bg-white">
             <div className="max-w-(--breakpoint-xl) mx-auto flex gap-2 justify-center lg:justify-start items-center h-full p-3 lg:p-5">
                 <p >Powered by</p>
-                <Link href="/">
+                <Link href={process.env.NEXT_PUBLIC_APP_URL!}>
                     <span className={cn("text-2xl font-semibold", poppins.className)}>
                         Glimroad
                     </span>
